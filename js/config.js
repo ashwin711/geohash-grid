fdafb639d043cbdc8270a79aec61958c9beb6d49
Sooraj <sooraj@adnear.com>
2016-08-02 21:30:47 +0530

    ah! wrong file in gitignore

---
+++ js/config.js
@@ -0,0 +1,3 @@
+var googleMapsSrc = "https://maps.googleapis.com/maps/api/js?v=3&libraries=places&key=" + googleMapsKey;
+document.write('<' + 'script src="' + googleMapsSrc + '"' +
+                   ' type="text/javascript"><' + '/script>');
